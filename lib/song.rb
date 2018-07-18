1de918634979cb36deb35181783030e0ce98ce27
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-07-18 00:38:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,9 @@ class Song
   @@count = 0
 
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
+    #@name = name
+    #@artist = artist
+    #@genre = genre
     @@count += 1
   end
 
