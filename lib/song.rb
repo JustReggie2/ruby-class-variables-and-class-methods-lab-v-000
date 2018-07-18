4eb797d8c31e90ab5c394498d7b7b57468ea7e21
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-07-18 00:44:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,13 +4,15 @@ class Song
 
   @@count = 0
   @@genres = []
-
+  @@artists = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
     @@genres << genre
+    @@artists << artist
   end
 
   def self.count
